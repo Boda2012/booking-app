1b9fda6365ad03030f4b59e012fd8d23fca9f512
Bogoljub Simonovic <bogoljub2012@gmail.com>
2024-07-07 11:07:38 +0200

    added hotel views

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -11,7 +11,7 @@ test.beforeEach(async ({ page }) => {
 
   await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
 
-  await page.locator("[name=email]").fill("2@22.com");
+  await page.locator("[name=email]").fill("zoki@smoki.com");
   await page.locator("[name=password]").fill("password");
 
   await page.getByRole("button", { name: "Login" }).click();
@@ -28,16 +28,16 @@ test("should allow user to add a hotel", async ({ page }) => {
   await page
     .locator('[name="description"]')
     .fill("This is a description for the Test Hotel");
-  await page.locator('[name="pricePerNight"]').fill("150");
-  await page.selectOption('select[name="starRating"]', "4");
+  await page.locator('[name="pricePerNight"]').fill("119");
+  await page.selectOption('select[name="starRating"]', "2");
 
   await page.getByText("Budget").click();
 
   await page.getByLabel("Free Wifi").check();
   await page.getByLabel("Parking").check();
 
-  await page.locator('[name="adultCount"]').fill("3");
-  await page.locator('[name="childCount"]').fill("5");
+  await page.locator('[name="adultCount"]').fill("2");
+  await page.locator('[name="childCount"]').fill("3");
 
   await page.setInputFiles('[name="imageFiles"]', [
     path.join(__dirname, "files", "1.png"),
@@ -55,9 +55,9 @@ test("should display hotels", async ({ page }) => {
   await expect(page.getByText("Lorem ipsum dolor sit amet")).toBeVisible();
   await expect(page.getByText("Dublin, Ireland")).toBeVisible();
   await expect(page.getByText("All Inclusive")).toBeVisible();
-  await expect(page.getByText("£150 per night")).toBeVisible();
-  await expect(page.getByText("3 adults, 5 children")).toBeVisible();
-  await expect(page.getByText("5 Star Rating")).toBeVisible();
+  await expect(page.getByText("£119 per night")).toBeVisible();
+  await expect(page.getByText("2 adults, 3 children")).toBeVisible();
+  await expect(page.getByText("2 Star Rating")).toBeVisible();
 
   await expect(
     page.getByRole("link", { name: "View Details" }).first()
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
-// import MyHotels from "./pages/MyHotels";
+import MyHotels from "./pages/MyHotels";
 // import EditHotel from "./pages/EditHotel";
 // import Search from "./pages/Search";
 // import Detail from "./pages/Detail";
@@ -81,14 +81,25 @@ const App = () => {
         />
       
         */}
-        {isLoggedIn && <Route
+        {isLoggedIn && <>
+        <Route
               path="/add-hotel"
               element={
                 <Layout>
                   <AddHotel />
                 </Layout>
               }
-            />}
+            />
+            <Route
+              path="/my-hotels"
+              element={
+                <Layout>
+                  <MyHotels />
+                </Layout>
+              }
+            />
+            </>
+            }
 
         {/* {isLoggedIn && (
           <>
